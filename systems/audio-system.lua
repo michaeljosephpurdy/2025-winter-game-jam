bdb630e94e868c1f00732c2fd317299683cf3cb3
Michael Purdy <michaeljosephpurdy@gmail.com>
2025-02-28 09:24:29 -0500

    Add toggle for audio, set music to looping

--- systems/audio-system.lua
+++ systems/audio-system.lua
@@ -1,7 +1,8 @@
 local AudioSystem = tiny.processingSystem()
 AudioSystem.filter = tiny.requireAll('audio')
 
-function AudioSystem:initialize()
+---@param props SystemProps
+function AudioSystem:initialize(props)
   self.sources = {
     MUSIC = love.audio.newSource('assets/music.wav', 'static'),
     WIN = love.audio.newSource('assets/audio_sfx_2.wav', 'static'),
@@ -10,7 +11,9 @@ function AudioSystem:initialize()
     LONG_JUMP = love.audio.newSource('assets/audio_sfx_3.wav', 'static'),
     DIE = love.audio.newSource('assets/audio_sfx_4.wav', 'static'),
   }
-  love.audio.setVolume(0.05)
+  self.volume = 0.05
+  love.audio.setVolume(self.volume)
+  self.keyboard_state = props.keyboard_state
 end
 
 function AudioSystem:onAddToWorld()
@@ -19,13 +22,25 @@ function AudioSystem:onAddToWorld()
   self.world:addEntity(bg_music)
 end
 
+function AudioSystem:postWrap()
+  if self.keyboard_state:is_key_just_released('m') then
+    if love.audio.getVolume() > 0 then
+      love.audio.setVolume(0)
+    else
+      love.audio.setVolume(self.volume)
+    end
+  end
+end
+
 ---@param e Audio
 function AudioSystem:onAdd(e)
   local source = self.sources[e.audio]
-  source:play()
-  if e.audio ~= 'MUSIC' then
+  if e.audio == 'MUSIC' then
+    source:setLooping(true)
+  else
     self.world:removeEntity(e)
   end
+  source:play()
 end
 
 function AudioSystem:onEntityRemove(e)
